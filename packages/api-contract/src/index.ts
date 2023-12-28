df4fbb9f8877e68c2c8610800e32251f1e3f0d11
catannoyance <147043079+catannoyance@users.noreply.github.com>
2023-12-29 01:25:15 +0300

    add server

---
+++ packages/api-contract/src/index.ts
@@ -0,0 +1,55 @@
+import { initContract } from "@ts-rest/core";
+import { z } from "zod";
+import { validationSchemas } from "@parkingspaces/db/validation";
+
+const c = initContract();
+
+export const contract = c.router(
+	{
+		getParkingSpaces: c.query({
+			path: "/parking-spaces",
+			method: "GET",
+			responses: {
+				200: z.array(validationSchemas.parkingSpace.select),
+			},
+		}),
+
+		getParkingSpace: c.query({
+			path: "/parking-space/:id",
+			method: "GET",
+			pathParams: z.object({
+				id: z.string().transform(Number),
+			}),
+			responses: {
+				200: validationSchemas.parkingSpace.select,
+				404: z.undefined(),
+			},
+		}),
+
+		createParkingSpace: c.mutation({
+			path: "/parking-space",
+			method: "POST",
+			body: validationSchemas.parkingSpace.create,
+			responses: {
+				200: validationSchemas.parkingSpace.select,
+				404: z.undefined(),
+			},
+		}),
+
+		updateParkingSpace: c.mutation({
+			path: "/parking-space/:id",
+			method: "PATCH",
+			pathParams: z.object({
+				id: z.string().transform(Number),
+			}),
+			body: validationSchemas.parkingSpace.create.partial(),
+			responses: {
+				200: validationSchemas.parkingSpace.select,
+				404: z.undefined(),
+			},
+		}),
+	},
+	{
+		strictStatusCodes: true,
+	},
+);
---
+++ packages/server/src/index.ts
@@ -0,0 +1,87 @@
+import express from "express";
+import morgan from "morgan";
+import { contract } from "@parkingspaces/api-contract";
+import { initServer, createExpressEndpoints } from "@ts-rest/express";
+import { db } from "./db";
+import { parkingSpace } from "@parkingspaces/db/schema";
+import { Point } from "@parkingspaces/db/types";
+import { eq, sql } from "drizzle-orm";
+const app = express();
+
+app.use(morgan("combined"));
+
+const s = initServer();
+
+const selectParkingSpace = () => ({
+	id: parkingSpace.id,
+	name: parkingSpace.name,
+	address: parkingSpace.address,
+	coords: sql<Point>`ST_AsText(${parkingSpace.coords})`.mapWith(parkingSpace.coords),
+	maxSlots: parkingSpace.maxSlots,
+	locationType: parkingSpace.locationType,
+	paymentType: parkingSpace.paymentType,
+	ownershipType: parkingSpace.ownershipType,
+});
+
+const router = s.router(contract, {
+	createParkingSpace: async ctx => ({
+		status: 200,
+		body: (
+			await db
+				.insert(parkingSpace)
+				.values(ctx.body)
+				.returning(selectParkingSpace())
+				.execute()
+		)[0],
+	}),
+	
+	getParkingSpace: async ctx => {
+		const result = await db
+			.select(selectParkingSpace())
+			.from(parkingSpace)
+			.where(eq(parkingSpace.id, ctx.params.id))
+			.execute();
+
+		if (result.length === 0) {
+			return {
+				status: 404,
+			};
+		}
+
+		return {
+			status: 200,
+			body: result[0],
+		};
+	},
+
+	getParkingSpaces: async _ctx => ({
+		status: 200,
+		body: await db.select(selectParkingSpace()).from(parkingSpace).execute(),
+	}),
+
+	updateParkingSpace: async req => {
+		const result = await db
+			.update(parkingSpace)
+			.set(req.body)
+			.where(eq(parkingSpace.id, req.body.id!))
+			.returning(selectParkingSpace())
+			.execute();
+
+		if (result.length === 0) {
+			return {
+				status: 404,
+			};
+		}
+
+		return {
+			status: 200,
+			body: result[0],
+		};
+	},
+});
+
+createExpressEndpoints(contract, router, app);
+
+app.listen(8000, () => {
+	console.log("Listening on port 8000");
+});
