374aa5d285362fde73e0942ada4f9efdc17c19f1
catannoyance <147043079+catannoyance@users.noreply.github.com>
2023-12-29 00:23:25 +0300

    hi :з

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+	},
+	extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+	overrides: [
+		{
+			env: {
+				node: true,
+			},
+			files: [".eslintrc.{js,cjs}"],
+			parserOptions: {
+				sourceType: "script",
+			},
+		},
+	],
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		ecmaVersion: "latest",
+		sourceType: "module",
+	},
+	plugins: ["@typescript-eslint", "unused-imports"],
+	rules: {
+		"@typescript-eslint/no-unused-vars": "off",
+		"unused-imports/no-unused-imports": [
+			"warn",
+			{
+				vars: "all",
+				varsIgnorePattern: "^_",
+			},
+		],
+		"arrow-body-style": "warn",
+	},
+};
