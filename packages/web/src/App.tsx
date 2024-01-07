b3a579b6c82e47e65aa975dcb9f35451c2f045f7
catannoyance <147043079+catannoyance@users.noreply.github.com>
2024-01-07 16:33:28 +0300

    add routing

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,12 +1,27 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import { ParkingSpacesList } from "./components/ParkingSpaceList"
+import { ParkingSpacesList } from "./routes/ParkingSpaceList"
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom"
+import { ParkingSpaceDisplay } from "./routes/ParkingSpaceDisplay"
 
 const queryClient = new QueryClient()
+const router = createBrowserRouter([
+	{
+		element: (
+			<div className="p-6">
+				<Outlet />
+			</div>
+		),
+		children: [
+			{
+				path: "/",
+				element: <ParkingSpacesList />,
+			},
+		],
+	},
+])
 export const App = () => (
 	<QueryClientProvider client={queryClient}>
-		<div className="p-6">
-			<ParkingSpacesList />
-		</div>
+		<RouterProvider router={router} />
 	</QueryClientProvider>
 )
 
