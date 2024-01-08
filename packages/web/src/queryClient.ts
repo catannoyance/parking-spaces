a7dfc9be07aa84a96bf4e1adaad65e3ee7172ef7
catannoyance <147043079+catannoyance@users.noreply.github.com>
2024-01-08 20:40:47 +0300

    load api url from env variables

--- packages/web/src/queryClient.ts
+++ packages/web/src/queryClient.ts
@@ -1,7 +1,11 @@
 import { contract } from "@parkingspaces/api-contract"
 import { initQueryClient } from "@ts-rest/react-query"
 
+if (!import.meta.env.VITE_API_URL) {
+	throw new Error("VITE_API_URL env variable is not set")
+}
+
 export const restQueryClient = initQueryClient(contract, {
-	baseUrl: "http://localhost:8000",
+	baseUrl: import.meta.env.VITE_API_URL,
 	baseHeaders: {},
 })
